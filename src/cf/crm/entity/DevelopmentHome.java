c91a85b422479284114a18ac95dbd97f57aa097f
SabrinaWang <673458347@qq.com>
2014-07-12 16:48:31 +0800

    Signed-off-by: SabrinaWang <673458347@qq.com>

---
+++ src/cf/crm/entity/DevelopmentHome.java
@@ -0,0 +1,124 @@
+package cf.crm.entity;
+
+// Generated 2014-7-12 16:42:52 by Hibernate Tools 3.6.0
+
+import java.util.List;
+import javax.naming.InitialContext;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.hibernate.LockMode;
+import org.hibernate.SessionFactory;
+import org.hibernate.criterion.Example;
+
+/**
+ * Home object for domain model class Development.
+ * @see src.cf.crm.entity.Development
+ * @author Hibernate Tools
+ */
+public class DevelopmentHome {
+
+	private static final Log log = LogFactory.getLog(DevelopmentHome.class);
+
+	private final SessionFactory sessionFactory = getSessionFactory();
+
+	protected SessionFactory getSessionFactory() {
+		try {
+			return (SessionFactory) new InitialContext()
+					.lookup("SessionFactory");
+		} catch (Exception e) {
+			log.error("Could not locate SessionFactory in JNDI", e);
+			throw new IllegalStateException(
+					"Could not locate SessionFactory in JNDI");
+		}
+	}
+
+	public void persist(Development transientInstance) {
+		log.debug("persisting Development instance");
+		try {
+			sessionFactory.getCurrentSession().persist(transientInstance);
+			log.debug("persist successful");
+		} catch (RuntimeException re) {
+			log.error("persist failed", re);
+			throw re;
+		}
+	}
+
+	public void attachDirty(Development instance) {
+		log.debug("attaching dirty Development instance");
+		try {
+			sessionFactory.getCurrentSession().saveOrUpdate(instance);
+			log.debug("attach successful");
+		} catch (RuntimeException re) {
+			log.error("attach failed", re);
+			throw re;
+		}
+	}
+
+	public void attachClean(Development instance) {
+		log.debug("attaching clean Development instance");
+		try {
+			sessionFactory.getCurrentSession().lock(instance, LockMode.NONE);
+			log.debug("attach successful");
+		} catch (RuntimeException re) {
+			log.error("attach failed", re);
+			throw re;
+		}
+	}
+
+	public void delete(Development persistentInstance) {
+		log.debug("deleting Development instance");
+		try {
+			sessionFactory.getCurrentSession().delete(persistentInstance);
+			log.debug("delete successful");
+		} catch (RuntimeException re) {
+			log.error("delete failed", re);
+			throw re;
+		}
+	}
+
+	public Development merge(Development detachedInstance) {
+		log.debug("merging Development instance");
+		try {
+			Development result = (Development) sessionFactory
+					.getCurrentSession().merge(detachedInstance);
+			log.debug("merge successful");
+			return result;
+		} catch (RuntimeException re) {
+			log.error("merge failed", re);
+			throw re;
+		}
+	}
+
+	public Development findById(java.lang.String id) {
+		log.debug("getting Development instance with id: " + id);
+		try {
+			Development instance = (Development) sessionFactory
+					.getCurrentSession().get("src.cf.crm.entity.Development",
+							id);
+			if (instance == null) {
+				log.debug("get successful, no instance found");
+			} else {
+				log.debug("get successful, instance found");
+			}
+			return instance;
+		} catch (RuntimeException re) {
+			log.error("get failed", re);
+			throw re;
+		}
+	}
+
+	public List findByExample(Development instance) {
+		log.debug("finding Development instance by example");
+		try {
+			List results = sessionFactory.getCurrentSession()
+					.createCriteria("src.cf.crm.entity.Development")
+					.add(Example.create(instance)).list();
+			log.debug("find by example successful, result size: "
+					+ results.size());
+			return results;
+		} catch (RuntimeException re) {
+			log.error("find by example failed", re);
+			throw re;
+		}
+	}
+}
