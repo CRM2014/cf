fea5c3ae3932b8d7d755c59ba08df8d4cd264ea3
陈释荣 <11301119@bjtu.edu.cn>
2014-07-19 16:49:55 +0800

    Signed-off-by: 陈释荣 <11301119@bjtu.edu.cn> 修改营销管理中的ChanceAction和PlanAction，慎合。。。

--- src/cf/crm/action/marketing/PlanAction.java
+++ src/cf/crm/action/marketing/PlanAction.java
@@ -1,9 +1,18 @@
 package cf.crm.action.marketing;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Controller;
 
 import cf.crm.action.BaseAction;
+import cf.crm.entity.Development;
+import cf.crm.entity.Salechance;
+import cf.crm.entity.User;
+import cf.crm.service.DevelopmentService;
+import cf.crm.service.SalechanceService;
+import cf.crm.service.UserService;
+import cf.crm.util.page.Page;
 
 @Controller
 @Scope("prototype")
@@ -13,7 +22,13 @@ public class PlanAction extends BaseAction {
 	 * 
 	 */
 	private static final long serialVersionUID = -2534997130378873595L;
-	
+	@Autowired
+	@Qualifier("developmentServiceImpl")
+	private DevelopmentService developmentService;
+	private Page<Development> page;
+	private Development Development;
+	private Development condition;
+
 	@Override
 	public String execute() throws Exception {
 		return "fail";
@@ -29,7 +44,7 @@ public class PlanAction extends BaseAction {
 		return "success";
 	}
 	public String fail(){
-		return "fail";
+		return "fail";   
 	}
 	public String make(){
 		return "make";
