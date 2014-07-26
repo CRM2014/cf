35883b4affc679273326aa073bb4d1829ad12280
齐宇 <11301127@bjtu.edu.cn>
2014-07-26 14:38:40 +0800

    junit

--- test/cf/crm/dao/impl/DaoAdapterTest.java
+++ test/cf/crm/dao/impl/DaoAdapterTest.java
@@ -11,10 +11,6 @@
 
 package cf.crm.dao.impl;
 
-import java.util.ArrayList;
-import java.util.Date;
-import java.util.List;
-
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -24,8 +20,6 @@ import org.springframework.test.context.transaction.TransactionConfiguration;
 import org.springframework.transaction.annotation.Transactional;
 
 import cf.crm.entity.Storage;
-import cf.crm.entity.User;
-import cf.crm.util.Info.Role;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(locations = "classpath:config/applicationContext.xml")
@@ -35,30 +29,11 @@ public class DaoAdapterTest {
 	@Autowired
 	private DaoAdapter daoAdapter;
 
-	public void test() {
-		List<Storage> list = new ArrayList<>();
-		for (int i = 0; i < 1000000; i++) {
-			Storage storage = new Storage();
-			storage.setStId(daoAdapter.generateKey());
-			storage.setStName(daoAdapter.generateKey());
-			list.add(storage);
-		}
-		daoAdapter.add(list);
-	}
-
 	@Test
-	public void test2() {
-		List<User> list = new ArrayList<>();
-		for (int i = 0; i < 100000; i++) {
-			User user = new User();
-			user.setUsId(daoAdapter.generateKey());
-			user.setUsRole(Role.SENIOR);
-			user.setUsName(daoAdapter.generateKey(7));
-			user.setUsUserName(daoAdapter.generateKey(7));
-			user.setUsPassword("666666");
-			user.setUsCreateTime(new Date());
-			list.add(user);
-		}
-		daoAdapter.add(list);
+	public void addTest() {
+		Storage storage = new Storage();
+		storage.setStId("1");
+		storage.setStName("2");
+		daoAdapter.add(storage);
 	}
 }
